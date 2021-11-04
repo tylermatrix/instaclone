cec58c4ef527ec30c81f9ddd13ce4e87955cdd05
tylermatrix <tylermaatrix@gmail.com>
2021-11-04 15:08:56 -0400

    updated header
    played with tailwind and added breakpoints for the logo as well as other basic styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,15 +3,28 @@ import Image from "next/image";
 function Header() {
   return (
     <div>
-      <h1>I am a header</h1>
-
-      {/* Left */}
-      <div>
-        <Image src="https://links.papareact.com/ocw" layout="fill"></Image>
+      <div className="flex justify-between bg-white max-w-6xl">
+        {" "}
+        {/* using Tailwind to style */}
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid w-24 h-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit=""
+          ></Image>
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain" // Preserves aspect raetio
+          ></Image>
+        </div>
+        {/* Middle */}
+        <h1>Hello World</h1>
+        {/* Right */}
       </div>
-      {/* Middle */}
-
-      {/* Right */}
     </div>
   );
 }
